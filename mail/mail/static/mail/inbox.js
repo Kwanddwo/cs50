bb267c0a3e6a691e8220e1db74c58601c19e7196
Achraf <lamghari@kmprojets.ma>
2023-07-09 14:57:26 +0100

    Finished Project

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
+  /*window.onpopstate = (event) => {
+    if ('compose' in event.state)
+      compose_email();
+    else if ('mailbox' in event.state)
+      load_mailbox(event.state.mailbox);
+    else if ('email' in event.state)
+      load_email(event.state.email);
+  }*/
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -6,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').onclick = () => load_mailbox('archive');
   document.querySelector('#compose').onclick = () => compose_email();
 
-  // Compose form submition
+  // Compose form submission
   document.querySelector('#compose-form').onsubmit = () => send_email();
 
   // By default, load the inbox
@@ -14,6 +22,9 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function compose_email() {
+  // Add history
+  //history.pushState({compose: true}, "", "compose");
+
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -26,6 +37,9 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox, message) {
+  // Add history
+  //history.pushState({mailbox: mailbox}, "", mailbox);
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -46,7 +60,6 @@ function load_mailbox(mailbox, message) {
   .then(request => request.json())
   .then(emails => {
     for (email of emails) {
-      console.log(email);
       render_email(email);
     }
   });
@@ -61,9 +74,9 @@ function render_email(email) {
     <div class="col-auto">${email.subject}</div>
     <div class="col-auto ml-auto text-muted">${email.timestamp}</div>
     `;
-  // Unread emails should be white, read ones should be greyed
+  // Turn read emails grey
   if (email.read) {
-    email_box.classList.add('bg-light');
+    email_box.style.backgroundColor = 'whitesmoke';
   }
   email_box.onclick = () => load_email(email.id);
   document.querySelector('#emails-view').append(email_box);
@@ -71,6 +84,8 @@ function render_email(email) {
 
 // Loads the big email view, this is the onclick property for the small email boxes
 function load_email(email_id) {
+  //history.pushState({email: email_id}, "", `email=${email_id}`);
+
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
   document.querySelector('#big-email-view').innerHTML = '';
@@ -98,11 +113,50 @@ function load_email(email_id) {
       </div>
       <div class='m-3'>${email.body}</div>
       <div class='row'>
-        <div class='col-auto ml-auto'>Sent to: ${email.recipients}</div>
+        <div class='col-auto ml-auto'>Sent to: ${`${email.recipients}`.replace(',', ', ')}</div>
       </div>
       `;
+    
+    const buttons_div = document.createElement('div');
+    buttons_div.className = 'row mt-3';
+    buttons_div.id = 'big-email-buttons';
+      
+    const archive_button = document.createElement('button');
+    archive_button.className = (email.archived) ? 'btn btn-danger col-auto ml-auto mr-2' : 'btn btn-primary col-auto ml-auto mr-2';
+    archive_button.id = 'email-button-archive';
+    archive_button.innerHTML = (email.archived) ? 'Unarchive' : 'Archive';
+    archive_button.onclick = () => {
+      fetch(`emails/${email_id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          archived: (!email.archived)
+        })
+      })
+      .then(response => load_mailbox('inbox'));
+    }
+  
+    const reply_button = document.createElement('button');
+    reply_button.className = "btn btn-primary col-auto"
+    reply_button.id = 'email-button-reply';
+    reply_button.innerHTML = 'Reply';
+    reply_button.onclick = () => {
+      compose_email();
+      document.querySelector('#compose-recipients').value = email.sender;
+      if (email.body) {
+        document.querySelector('#compose-body').value = `On ${email.timestamp} ${email.sender} wrote: ${email.body}`;
+      }
+      const reply_expression = /^Re:.+/;
+      if (!email.subject.match(reply_expression)) {
+        document.querySelector('#compose-subject').value = 'Re: ' + email.subject;
+      } else {
+        document.querySelector('#compose-subject').value = email.subject;
+      }
 
-    document.querySelector('#big-email-view').append(big_email_box)
+    }
+
+    buttons_div.append(archive_button, reply_button);
+
+    document.querySelector('#big-email-view').append(big_email_box, buttons_div);
   })
 }
 
